dd84b7a8847db78285a0b6d3e0e430d194cb6e57
Gr-monaco <gr-monaco@hotmail.com>
2022-02-22 22:47:29 -0300

    Tag script colocado para baixo do documento. Tu é besta né

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+const head = document.querySelector(".header"); //Lembre que é uma lista
+
+
 document.addEventListener("click", function(e) {
-    console.log(e.target)
     if(e.target.classList.contains("buttonCalcular")){
         
     } 
 } )
+
+function GetTime(){
+    var time = document.createElement("p");
+    time.textContent = "teste";
+}
+
+console.log(head)
